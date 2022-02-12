2a9277fd9ae177281ab6d2d2eb6bd8cc2ccc0907
Laura <law7676@outlook.com>
2022-02-12 16:06:08 -0600

    modified all files

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const h3 = document.querySelectorAll('h3');
 const hiddenContent = document.querySelectorAll('.hidden');
+const button = document.querySelectorAll('button');
 
 for (let i = 0; i < hiddenContent.length; i++){
   hiddenContent[i].style.display = 'none';
 }
 
-for (let i = 0; i < h3.length; i++){
-  h3[i].addEventListener('click', function(event){
+for (let i = 0; i < button.length; i++){
+  button[i].addEventListener('click', function(event){
   if (hiddenContent[i].style.display === "none"){
     hiddenContent[i].style.display = "block";
   } else {
